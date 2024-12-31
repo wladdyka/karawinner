f9cc58f4191a828a2a036fb88925d70ae96c5e12
Vlad M <wladdyka@gmail.com>
2024-12-31 18:57:59 +0500

    added keycodes

--- main.cpp
+++ main.cpp
@@ -2,14 +2,34 @@
 #include <iostream>
 #include <unordered_map>
 
+// Enum for keycodes
+enum KeyCode {
+    // Modifiers
+    RSHIFT = 0x36,
+    LSHIFT = 0x2A,
+    RCTRL = 0x1D,
+    RALT = 0x38,
+    RWIN = 0xE05B,
+    // Alphabet keys
+    J = 0x24,
+    L = 0x26,
+    I = 0x17,
+    K = 0x25,
+    // Arrow keys
+    LEFT_ARROW = 0xE04B,
+    RIGHT_ARROW = 0xE04D,
+    UP_ARROW = 0xE048,
+    DOWN_ARROW = 0xE050
+};
+
 // Helper to track modifier key states
 std::unordered_map<int, bool> modifierState;
 
-bool isModifierPressed(int code) {
+bool isModifierPressed(KeyCode code) {
     return modifierState.count(code) && modifierState[code];
 }
 
-void sendKey(InterceptionContext context, InterceptionDevice device, int keyCode, int keyState) {
+void sendKey(InterceptionContext context, InterceptionDevice device, KeyCode keyCode, int keyState) {
     InterceptionKeyStroke keystroke;
     keystroke.code = keyCode;
     keystroke.state = keyState;
@@ -31,48 +51,47 @@ int main() {
         auto *keystroke = reinterpret_cast<InterceptionKeyStroke *>(&stroke);
 
         // Track modifier keys (Shift, Ctrl, Right Windows)
-        if (keystroke->code == SCANCODE_RSHIFT || keystroke->code == SCANCODE_LSHIFT ||
-            keystroke->code == SCANCODE_RCTRL || keystroke->code == SCANCODE_RALT ||
-            keystroke->code == SCANCODE_RWIN) {
+        if (keystroke->code == RSHIFT || keystroke->code == LSHIFT ||
+            keystroke->code == RCTRL || keystroke->code == RALT ||
+            keystroke->code == RWIN) {
             modifierState[keystroke->code] = (keystroke->state & INTERCEPTION_KEY_DOWN) != 0;
         }
 
         // Right Windows + J -> Left Arrow
-        if (keystroke->code == SCANCODE_J && isModifierPressed(SCANCODE_RWIN)) {
-            if (isModifierPressed(SCANCODE_RSHIFT)) {
+        if (keystroke->code == J && isModifierPressed(RWIN)) {
+            if (isModifierPressed(RSHIFT)) {
                 // Highlight to the left
-                sendKey(context, device, SCANCODE_LEFT_ARROW, INTERCEPTION_KEY_DOWN);
+                sendKey(context, device, LEFT_ARROW, INTERCEPTION_KEY_DOWN);
             } else {
                 // Just move to the left
-                sendKey(context, device, SCANCODE_LEFT_ARROW, INTERCEPTION_KEY_DOWN);
-                sendKey(context, device, SCANCODE_LEFT_ARROW, INTERCEPTION_KEY_UP);
+                sendKey(context, device, LEFT_ARROW, INTERCEPTION_KEY_DOWN);
+                sendKey(context, device, LEFT_ARROW, INTERCEPTION_KEY_UP);
             }
             continue;
         }
 
         // Right Windows + L -> Right Arrow
-        if (keystroke->code == SCANCODE_L && isModifierPressed(SCANCODE_RWIN)) {
-            if (isModifierPressed(SCANCODE_RSHIFT)) {
+        if (keystroke->code == L && isModifierPressed(RWIN)) {
+            if (isModifierPressed(RSHIFT)) {
                 // Highlight to the right
-                sendKey(context, device, SCANCODE_RIGHT_ARROW, INTERCEPTION_KEY_DOWN);
+                sendKey(context, device, RIGHT_ARROW, INTERCEPTION_KEY_DOWN);
             } else {
                 // Just move to the right
-                sendKey(context, device, SCANCODE_RIGHT_ARROW, INTERCEPTION_KEY_DOWN);
-                sendKey(context, device, SCANCODE_RIGHT_ARROW, INTERCEPTION_KEY_UP);
+                sendKey(context, device, RIGHT_ARROW, INTERCEPTION_KEY_DOWN);
+                sendKey(context, device, RIGHT_ARROW, INTERCEPTION_KEY_UP);
             }
             continue;
         }
 
         // Right Windows + I -> Up Arrow
-        if (keystroke->code == SCANCODE_I && isModifierPressed(SCANCODE_RWIN)) {
-            sendKey(context, device, SCANCODE_UP_ARROW, keystroke->state);
+        if (keystroke->code == I && isModifierPressed(RWIN)) {
+            sendKey(context, device, UP_ARROW, keystroke->state);
             continue;
         }
 
         // Right Windows + K -> Down Arrow
-        if (keystroke->code == SCANCODE_K && isModifierPressed(SCANCODE_RWIN)) {
-        if (keystroke->code == SCANCODE_K && isModifierPressed(SCANCODE_RWIN)) {
-            sendKey(context, device, SCANCODE_DOWN_ARROW, keystroke->state);
+        if (keystroke->code == K && isModifierPressed(RWIN)) {
+            sendKey(context, device, DOWN_ARROW, keystroke->state);
             continue;
         }
 

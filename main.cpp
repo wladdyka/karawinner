42337c7ed7b583ab7ec736e63cb7dcace01680d6
Vlad M <wladdyka@gmail.com>
2025-01-03 09:50:01 +0500

    finally works with right alt

--- main.cpp
+++ main.cpp
@@ -8,70 +8,71 @@ namespace scancode {
     enum {
         esc  = 0x01,
         ctrl = 0x1D,
-        //alt  = 0x38,
-        j = 36,
+        alt  = 0x38,
+        del  = 0x53,
+        j  = 36,
         lwin = 91,
         rwin = 92,
         home = 71,
+        ralt = 56,
     };
 }
 
-InterceptionKeyStroke rWinDown = {scancode::rwin, INTERCEPTION_KEY_DOWN | INTERCEPTION_KEY_E0, 0};
-InterceptionKeyStroke jDown  = {scancode::j , INTERCEPTION_KEY_DOWN, 0};
-InterceptionKeyStroke lWinDown  = {scancode::lwin , INTERCEPTION_KEY_DOWN | INTERCEPTION_KEY_E0, 0};
+void hueta(InterceptionContext context, InterceptionDevice device, short keyCode, int keyState) {
+    InterceptionKeyStroke keystroke;
+    keystroke.code = keyCode;
+    keystroke.state = keyState;
 
-InterceptionKeyStroke rWinUp   = {scancode::rwin, INTERCEPTION_KEY_UP | INTERCEPTION_KEY_E0, 0};
-InterceptionKeyStroke jUp    = {scancode::j , INTERCEPTION_KEY_UP, 0};
-InterceptionKeyStroke lWinUp    = {scancode::lwin , INTERCEPTION_KEY_UP | INTERCEPTION_KEY_E0, 0};
+    interception_send(context, device, reinterpret_cast<const InterceptionStroke*>(&keystroke), 1);
+}
+
+InterceptionKeyStroke lwinDown = {scancode::lwin, INTERCEPTION_KEY_DOWN | INTERCEPTION_KEY_E0, 0};
+InterceptionKeyStroke raltDown  = {scancode::alt , INTERCEPTION_KEY_DOWN | INTERCEPTION_KEY_E0, 0};
+InterceptionKeyStroke jDown  = {scancode::j , INTERCEPTION_KEY_DOWN, 0};
+
+InterceptionKeyStroke lwinUp   = {scancode::lwin, INTERCEPTION_KEY_UP | INTERCEPTION_KEY_E0, 0};
+InterceptionKeyStroke raltUp    = {scancode::alt , INTERCEPTION_KEY_UP  | INTERCEPTION_KEY_E0, 0};
+InterceptionKeyStroke jUp    = {scancode::j , INTERCEPTION_KEY_UP , 0};
 
 bool operator==(const InterceptionKeyStroke &first,
                 const InterceptionKeyStroke &second) {
     return first.code == second.code && first.state == second.state;
 }
 
-bool shall_produce_keystroke(const InterceptionKeyStroke &kstroke) {
-    static int rWinIsDown = 0, jIsDown = 0, lWinIsDown = 0;
+bool homeShortcut(const InterceptionKeyStroke &kstroke) {
+    static int lwin = 0, ralt = 0, j = 0;
 
-    //cout << "sum:" << ctrl_is_down + rwin_is_down + lwin_is_down << endl;
-    if (rWinIsDown + jIsDown + lWinIsDown < 2) {
-        if (kstroke == rWinDown) { rWinIsDown = 1; }
-        if (kstroke == rWinUp  ) { rWinIsDown = 0; }
-        if (kstroke == jDown ) { jIsDown = 1;  }
-        if (kstroke == jUp   ) { jIsDown = 0;  }
-        if (kstroke == lWinDown ) { lWinIsDown = 1;  }
-        if (kstroke == lWinUp   ) { lWinIsDown = 0;  }
+    if (lwin + ralt + j < 2) {
+        if (kstroke == lwinDown) { lwin = 1; }
+        if (kstroke == lwinUp  ) { lwin = 0; }
+        if (kstroke == raltDown ) { ralt = 1;  }
+        if (kstroke == raltUp   ) { ralt = 0;  }
+        if (kstroke == jDown ) { j = 1;  }
+        if (kstroke == jUp   ) { j = 0;  }
         return true;
     }
 
-    if (rWinIsDown == 0 && (kstroke == rWinDown || kstroke == rWinUp)) {
+    if (lwin == 0 && (kstroke == lwinDown || kstroke == lwinUp)) {
         return false;
     }
 
-    if (jIsDown == 0 && (kstroke == jDown || kstroke == jUp)) {
+    if (ralt == 0 && (kstroke == raltDown || kstroke == raltUp)) {
         return false;
     }
 
-    if (lWinIsDown == 0 && (kstroke == lWinDown || kstroke == lWinUp)) {
+    if (j == 0 && (kstroke == jDown || kstroke == jUp)) {
         return false;
     }
 
-    if (kstroke == rWinUp) {
-        rWinIsDown = 0;
+    if (kstroke == lwinUp) {
+        lwin = 0;
+    } else if (kstroke == raltUp) {
+        ralt = 0;
     } else if (kstroke == jUp) {
-        jIsDown = 0;
-    } else if (kstroke == lWinUp) {
-        lWinIsDown = 0;
+        j = 0;
     }
 
     return true;
-}
-
-void sendKey(InterceptionContext context, InterceptionDevice device, short keyCode, int keyState) {
-    InterceptionKeyStroke keystroke;
-    keystroke.code = keyCode;
-    keystroke.state = keyState;
-
-    interception_send(context, device, reinterpret_cast<const InterceptionStroke*>(&keystroke), 1);
 }
 
 int main() {
@@ -86,24 +87,18 @@ int main() {
 
     while (interception_receive(context, device = interception_wait(context),
                                 (InterceptionStroke *)&kstroke, 1) > 0) {
-        //cout << kstroke.code << " " << kstroke.state << endl;
+        //std::cout << kstroke.code << " " << kstroke.state << std::endl;
 
-        if (!shall_produce_keystroke(kstroke)) {
-            cout << "home shorcut triggered" << endl;
-            sendKey(context, device, scancode::home, INTERCEPTION_KEY_DOWN);
-            sendKey(context, device, scancode::home, INTERCEPTION_KEY_UP);
+        if (!homeShortcut(kstroke)) {
+            cout << "homeShortcut()" << endl;
+            hueta(context, device, scancode::home, INTERCEPTION_KEY_DOWN);
+            hueta(context, device, scancode::home, INTERCEPTION_KEY_UP);
             continue;
         }
 
-        if (kstroke.code == scancode::lwin)
-        {
-            continue;
-        }
-
-        if (kstroke.code == scancode::rwin)
-        {
-            continue;
-        }
+        if (kstroke.code == scancode::alt) continue;
+        if (kstroke.code == scancode::ralt) continue;
+        if (kstroke.code == scancode::lwin) continue;
 
         interception_send(context, device, (InterceptionStroke *)&kstroke, 1);
 
